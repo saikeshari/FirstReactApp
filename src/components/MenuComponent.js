464ea2637d3662195f3c83b995d36ea678ad5192
Sai Keshari <saikeshari@gmail.com>
2020-10-10 02:22:27 +0530

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
 function RenderMenuItem({dish, onClick}) {
   return(
-    <Card onClick={() => onClick(dish.id)}>
+    <Card>
         <CardImg width="100%" object src={dish.image} alt={dish.name} />
         <CardImgOverlay body className="ml-5">
           <CardTitle>{dish.name}</CardTitle>
